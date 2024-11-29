9f66a5ed6d63b8a00494391b76697ec9a31ff3d5
Denny Rubino <rubidenny@gmail.com>
2024-11-29 13:12:59 +0100

    Add numeber rules

--- js/script.js
+++ js/script.js
@@ -8,7 +8,16 @@
 
 //Faccio numerare i da 1 al 100
 for (let i = 1; i <= 100; i++) 
-     console.log(i);
+    //Aggiungo delle regole
+if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+} else if (i % 3 === 0) {
+    console.log("Fizz");
+} else if (i % 5 === 0) {
+    console.log("Buzz");
+} else {
+    console.log(i);
+}
 
 
     
